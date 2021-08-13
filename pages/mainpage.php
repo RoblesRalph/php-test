3cac19a286a9f87008b51da9bc855cd150854690
RoblesRalph <61932828+RoblesRalph@users.noreply.github.com>
2021-08-13 12:23:27 +0800

    Update mainpage.php

--- pages/mainpage.php
+++ pages/mainpage.php
@@ -1,15 +1,10 @@
 <?php
   echo "Hello, World.<br><br>";
-
-  if ($_SERVER["REQUEST_METHOD"] ==  "POST")
-  {
-    echo "Welcome <?php echo $_POST['name']; ?><br>";
-  }
 ?>
 <br>
 <br>
-<form action="mainpage.php" method="post">
+<form action="greeting.php" method="post">
   <label for="name">Enter Your Name: </label> 
   <input type="text" name="name"><br>
-  <button>Give a greeting</button>
+  <button type="submit">Give a greeting</button>
 </form>
