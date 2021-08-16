a1d6a5d3157b60a9b0297f25b029c5bd5453b00d
RoblesRalph <61932828+RoblesRalph@users.noreply.github.com>
2021-08-16 10:42:13 +0800

    Update mainpage.php

--- pages/mainpage.php
+++ pages/mainpage.php
@@ -1,10 +1,19 @@
-<?php
-  echo "Hello, World.<br><br>";
-?>
-<br>
-<br>
-<form action="greeting.php" method="post">
-  <label for="name">Enter Your Name: </label> 
-  <input type="text" name="name"><br>
-  <button type="submit">Give a greeting</button>
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome</title>
+</head>
+<body>
+  <?php
+    echo "Hello, World.<br><br>";
+  ?>
+  <br>
+  <br>
+  <form action="greeting.php" method="post">
+    <label for="name">Enter Your Name: </label> 
+    <input type="text" name="name"><br>
+    <button type="submit">Give a greeting</button>
+  </form>
+</body>
+</html>
+
